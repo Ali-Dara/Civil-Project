d739ce11d8d761d8325b6718a565e8c7fa131d8d
alidara <alireza.irani.ad@gmail.com>
2021-01-09 11:49:31 +0330

    Adding All Change On Last Week Thursday

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@ $(window).scroll(function () {
     let scroll = $(window).scrollTop();
     if (scroll >= 15) {
         $(".navBar").addClass('navBg');
-        
+
     } else {
         $(".navBar").removeClass('navBg');
     }
@@ -10,23 +10,35 @@ $(window).scroll(function () {
 
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-    rtl:true,
-    items:4,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:2000,
-    autoplayHoverPause:true,
+    rtl: true,
+    items: 4,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
     dots: false,
-    responsive:{
-        0:{
-            items:1
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:5
+        1000: {
+            items: 5
         }
     }
 });
+
+$('.link').click(function () {
+    let id = $(this).attr("id");
+    $("#artImg").attr("src", "/Images/" + id + ".JPG");
+    if(!$(this).hasClass('activelink')){
+        $('.link').removeClass('activelink')
+        $(this).addClass('activelink')
+    }
+    else{
+        $('.link').removeClass('activelink')
+    }
+})
